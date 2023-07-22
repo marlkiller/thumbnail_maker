000525f5b83ebab0102803364fe82f8def6ba809
artemis <marlkiller@voidm.com>
2023-07-22 17:22:24 +0800

    1. Add thumbnail_maker.sh 2. Add README.md

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # thumbnail_maker
-makes thumbnails of videos
+
+This project is a command-line tool that generates x*y thumbnails for a given video file. Each thumbnail displays the time of the corresponding frame, and all the thumbnails are merged into a single large image. The resulting image also includes information about the video's size and duration.
+
+
+
+
+![](sample/one-piece.E0162.mkv_merge.png)
+
+# Features
+
+- Generates x*y thumbnails for a given video file.
+- Displays the time of each frame in the thumbnail.
+- Includes information about the video's size and duration in the resulting image.
+
+# Prerequisites
+- ffmpeg
+- jq
+
+To use this tool, you need to have ffmpeg installed on your system. You can install it by following the instructions on the [official website ↗.](https://ffmpeg.org/download.html)
+
+
+# Usage
+
+
+```shell
+git clone github.com/marlkiller/thumbnail_maker
+cd thumbnail_maker
+
+sh thumbnail_maker.sh sample/video.mp4 4 5
+```
+
+
+sh thumbnail_maker.sh [input_video] [x] [y]
+- input_video: /path/video.mp4
+- x y: rows and columns
+
+In this example, "4 5" splits each input stream into 4 rows and 5 columns, resulting in 20 equal IMG blocks in the output stream.
+
+
+
+## How it works?
+
+TODO
---
+++ thumbnail_maker.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+# Usage : sh thumbnail_maker.sh /Users/voidm/Downloads/one-piece.E0162.mkv 3 4
+
+param_count=$#
+if [ $param_count -lt 3 ]; then
+    echo "Error: Need at least 3 parameters"
+    echo "Usage: $0 video.mp4 3 4"
+    exit 1
+fi
+
+if ! command -v ffmpeg &>/dev/null; then
+    echo "Please install ffmpeg first"
+    exit 1
+fi
+
+if ! command -v jq &>/dev/null; then
+    echo "Please install jq first."
+    exit 1
+fi
+
+echo "Processing $0 $1 $2"
+
+abs_video_file="$1"
+video_name=$(basename "$abs_video_file")
+
+# ffmpeg config
+img_width=2048
+scale="scale=$img_width:-1,"
+img_limit="-frames:v 1 -update 1"
+ffmpeg_out=""
+#ffmpeg_out=">> ffmpeg.out.log 2>&1"
+
+## tile config
+x="$2"
+y="$3"
+total_count=$(expr $x \* $y)
+margin=50
+tile="tile=${x}x${y}:padding=20:margin=$margin:color=gray,"
+
+## time watermark config
+draw_time="drawtext=text='%{pts\:hms}':fontsize=h/15:fontcolor=white:x=w/20:y=h/20,"
+
+## Head info config
+info_height=130
+
+echo "$abs_video_file >>> [$x X $y = $total_count]"
+
+generate_tile_by_time() {
+    out_img_name="$abs_video_file""_""$1".png
+    rm -rf "$out_img_name"
+    total_time=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$abs_video_file")
+    # total_time=`ffprobe "$abs_video_file" -select_streams v -show_entries stream=duration -of default=nk=1:nw=1 -v quiet`
+    chunk=$(echo "scale=2; $total_time / ($total_count + 1)" | bc)
+    if (($(echo "$chunk <= 0" | bc -l))); then
+        chunk=0.01
+    fi
+    echo "total_time: $total_time, chunk: $chunk"
+    ffmpeg_cmd="ffmpeg -i \"$abs_video_file\" ${img_limit} -vf \"select=(gte(t\,$chunk))*(isnan(prev_selected_t)+gte(t-prev_selected_t\,$chunk)),${draw_time} ${tile} ${scale}\" -fps_mode auto \"$out_img_name\""
+    echo $ffmpeg_cmd
+    eval "$ffmpeg_cmd $ffmpeg_out"
+}
+
+size_format() {
+    total_size=$1
+    if [[ "$total_size" =~ ^[0-9]+$ ]]; then
+        if [ 1024 -gt $total_size ]; then
+            size="$total_size B"
+        elif [ 1048576 -gt $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 1024 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size KB"
+        elif [ 1073741824 -gt $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 1048576 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size MB"
+        elif [ 1073741824 -le $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 1073741824 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size GB"
+        else
+            size="0"
+        fi
+    else
+        size="NULL"
+    fi
+    echo $size
+}
+
+time_format() {
+    total_size=$1
+    if [[ $(echo "$total_size + 0" | bc) ]]; then
+        if [ 60 -gt $total_size ]; then
+            size="$total_size Sec"
+        elif [ 3600 -gt $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 60 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size Min"
+        elif [ 216000 -gt $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 3600 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size Hour"
+        elif [ 5184000 -le $total_size ]; then
+            size=$(echo "scale=2; a = $total_size / 216000 ; if (length(a)==scale(a)) print 0;print a" | bc)
+            size="$size Day"
+        else
+            size="0"
+        fi
+    else
+        size="NULL"
+    fi
+    echo $size
+}
+generate_video_info() {
+    out_img_name="$abs_video_file""_""$1".png
+    rm -rf "$out_img_name"
+    # 获取视频文件信息
+    info=$(ffprobe -v quiet -print_format json -show_format -show_streams "$abs_video_file")
+    filename=$(echo "$info" | jq -r '.format.filename')
+    size=$(echo "$info" | jq -r '.format.size')
+    duration=$(echo "$info" | jq -r '.format.duration')
+    width=$(echo "$info" | jq -r '.streams[0].width')
+    height=$(echo "$info" | jq -r '.streams[0].height')
+
+    size=$(size_format $size)
+    int_duration=${duration%.*}
+    duration=$(time_format $(echo "scale=0; $int_duration" | bc))
+
+    text_tile="$abs_video_file""_""$1".txt
+
+    cat >$text_tile <<EOF
+Filename: $video_name
+Size: $size
+Resolution: ${width}x${height}
+duration: ${duration}
+EOF
+    ffmpeg_cmd="ffmpeg  -f lavfi -i color=gray:s=${img_width}x${info_height}:d=1 -update 1  -filter:v  \"drawtext=textfile='$text_tile':fontsize=24:fontcolor=white:x=$margin/2:y=h/4\" \"$out_img_name\""
+    echo $ffmpeg_cmd
+    eval "$ffmpeg_cmd $ffmpeg_out"
+}
+
+tile_merge_info() {
+    file_img="$abs_video_file""_""$1".png
+    file_info="$abs_video_file""_""$2".png
+    file_merge="$abs_video_file""_""$3".png
+
+    rm -rf "$file_merge"
+
+    ffmpeg_cmd="ffmpeg -i \"$file_img\" -i \"$file_info\" -update 1 -frames:v 1 -filter_complex \"[0:v]pad=iw:ih+$info_height:0:$info_height:color=white[top]; [top][1:v]overlay=0:0\" \"$file_merge\""
+    echo $ffmpeg_cmd
+    eval "$ffmpeg_cmd $ffmpeg_out"
+}
+
+start_time=$(date +%s.%N)
+
+generate_tile_by_time "time_2"
+
+generate_video_info "info"
+
+tile_merge_info "time_2" "info" "merge"
+
+end_time=$(date +%s.%N)
+duration=$(echo "$end_time - $start_time" | bc)
+echo "[$video_name] Command take $duration seconds to execute."
